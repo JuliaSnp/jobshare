934a106b17da0e5db3add8aaad671f39dc0d6dd4
Julia <julia.snp@gmail.com>
2018-07-30 10:32:45 +0100

    search completed

--- job_share/search/urls.py
+++ job_share/search/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'search'
 urlpatterns = [
     url(r'^$',views.search, name='search'),
+
 ]
--- job_share/search/views.py
+++ job_share/search/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .filters import UserFilter
-
-
-def search(request):
-    user_list = User.objects.all()
-    user_filter = UserFilter(request.GET, queryset=user_list)
-    return render(request, 'search/user_list.html', {'filter': user_filter})
+from accounts.models import UserProfile
+#from .filters import UserFilter
+from .filters import ProfileFilter
+from accounts.views import view_profile, view_profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def search(request):
+                    #user_list = User.objects.all()
+     user_list = UserProfile.objects.all()
+                    #user_filter = UserFilter(request.GET, queryset=user_list)
+     userprofile_filter = ProfileFilter(request.GET, queryset=user_list)
+     return render(request, 'search/user_list.html', {'filter': userprofile_filter})
